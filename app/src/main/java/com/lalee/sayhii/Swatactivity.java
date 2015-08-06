83528e034a8a27edde9f68a330cc318192fa0139
swatida263 <swatida263@gmail.com>
2015-08-07 00:06:58 +0530

    First commit

---
+++ app/src/main/java/com/lalee/sayhii/Swatactivity.java
@@ -0,0 +1,12 @@
+package me.vable.android.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Swatactivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
